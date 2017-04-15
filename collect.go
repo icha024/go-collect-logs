75d335a87414181c4804a3d9c74ae792094214c3
Ian Chan <icha024@gmail.com>
2017-04-15 20:07:30 +0100

    Added SSE stream

--- collect.go
+++ collect.go
@@ -1,21 +1,25 @@
 package main
 
 import (
-	"bufio"
+	// "bufio"
 	"flag"
 	"fmt"
+	// "gopkg.in/gin-gonic/gin.v1"
+	"github.com/icha024/go-collect-logs/sse"
 	"gopkg.in/mcuadros/go-syslog.v2"
 	"gopkg.in/mcuadros/go-syslog.v2/format"
-	"os"
+	"log"
+	"net/http"
+	// "os"
 	"time"
 )
 
 func main() {
 	var maxLogEntries = flag.Int("max-log-entries", 10, "Maximum number of log entries to keep. Approx 1KB/entry.")
 	var logReadInteval = flag.Int("log-read-inteval", 3, "Interval, in seconds, to read syslog into memory.")
-	var syslogPort = flag.Int("syslog-port", 514, "Syslog port to listen on.")
+	var syslogPort = flag.Int("syslog-port", 10514, "Syslog port to listen on.")
 	var syslogHost = flag.String("syslog-host", "0.0.0.0", "Syslog host to listen on.")
-	var isParseSev = flag.Bool("sev", false, "Parse the syslog severity tag")
+	var isParseSev = flag.Bool("sev", false, "Parse the syslog severity header")
 	flag.Parse()
 
 	channel := make(syslog.LogPartsChannel)
@@ -32,7 +36,16 @@ func main() {
 	logArr := make([]string, *maxLogEntries, *maxLogEntries)
 	var writeIdx int
 	var readIdx int
+	broker := sse.NewServer()
 	fmt.Printf("Syslog collector started on: %s \n", sysLogServerDetail)
+
+	// router := gin.Default()
+	// router.GET("/ping", func(c *gin.Context) {
+	// 	c.JSON(200, gin.H{
+	// 		"message": "pong",
+	// 	})
+	// })
+	// router.Run() // listen and serve on 0.0.0.0:8080
 
 	go func(channel syslog.LogPartsChannel) {
 		var logEntry string
@@ -56,6 +69,7 @@ func main() {
 			case <-ticker.C:
 				for readIdx != writeIdx {
 					fmt.Printf(logArr[readIdx])
+					broker.Notifier <- []byte(logArr[readIdx])
 					readIdx++
 					if readIdx == *maxLogEntries {
 						readIdx = 0
@@ -66,11 +80,21 @@ func main() {
 		}
 	}()
 
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		text := scanner.Text()
-		fmt.Println(text)
-	}
+	// scanner := bufio.NewScanner(os.Stdin)
+	// for scanner.Scan() {
+	// 	text := scanner.Text()
+	// 	fmt.Println(text)
+	// }
+
+	// go func() {
+	// 	for {
+	// 		time.Sleep(time.Second * 2)
+	// 		eventString := fmt.Sprintf("the time is %v", time.Now())
+	// 		log.Println("Receiving event")
+	// 		broker.Notifier <- []byte(eventString)
+	// 	}
+	// }()
+	log.Fatal("HTTP server error: ", http.ListenAndServe("localhost:3000", broker))
 	server.Wait()
 }
 
