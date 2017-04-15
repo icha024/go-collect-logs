f87c3206500faf2979810aa1a16f06eda0ccd99b
Ian Chan <icha024@gmail.com>
2017-04-15 20:10:30 +0100

    Polishing

--- collect.go
+++ collect.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	// "bufio"
 	"flag"
 	"fmt"
-	// "gopkg.in/gin-gonic/gin.v1"
 	"github.com/icha024/go-collect-logs/sse"
 	"gopkg.in/mcuadros/go-syslog.v2"
 	"gopkg.in/mcuadros/go-syslog.v2/format"
@@ -17,8 +15,10 @@ import (
 func main() {
 	var maxLogEntries = flag.Int("max-log-entries", 10, "Maximum number of log entries to keep. Approx 1KB/entry.")
 	var logReadInteval = flag.Int("log-read-inteval", 3, "Interval, in seconds, to read syslog into memory.")
-	var syslogPort = flag.Int("syslog-port", 10514, "Syslog port to listen on.")
 	var syslogHost = flag.String("syslog-host", "0.0.0.0", "Syslog host to listen on.")
+	var syslogPort = flag.Int("syslog-port", 10514, "Syslog port to listen on.")
+	var host = flag.String("host", "0.0.0.0", "Service host to listen on.")
+	var port = flag.Int("port", 3000, "Service port to listen on.")
 	var isParseSev = flag.Bool("sev", false, "Parse the syslog severity header")
 	flag.Parse()
 
@@ -38,14 +38,6 @@ func main() {
 	var readIdx int
 	broker := sse.NewServer()
 	fmt.Printf("Syslog collector started on: %s \n", sysLogServerDetail)
-
-	// router := gin.Default()
-	// router.GET("/ping", func(c *gin.Context) {
-	// 	c.JSON(200, gin.H{
-	// 		"message": "pong",
-	// 	})
-	// })
-	// router.Run() // listen and serve on 0.0.0.0:8080
 
 	go func(channel syslog.LogPartsChannel) {
 		var logEntry string
@@ -80,21 +72,8 @@ func main() {
 		}
 	}()
 
-	// scanner := bufio.NewScanner(os.Stdin)
-	// for scanner.Scan() {
-	// 	text := scanner.Text()
-	// 	fmt.Println(text)
-	// }
-
-	// go func() {
-	// 	for {
-	// 		time.Sleep(time.Second * 2)
-	// 		eventString := fmt.Sprintf("the time is %v", time.Now())
-	// 		log.Println("Receiving event")
-	// 		broker.Notifier <- []byte(eventString)
-	// 	}
-	// }()
-	log.Fatal("HTTP server error: ", http.ListenAndServe("localhost:3000", broker))
+	serverDetail := fmt.Sprintf("%s:%d", *host, *port)
+	log.Fatal("HTTP server error: ", http.ListenAndServe(serverDetail, broker))
 	server.Wait()
 }
 
